07778496c7dd6d5d9f6640ed31ea79d229a2759f
Zichong Liu <144846450+Sleepwalker177@users.noreply.github.com>
2024-07-10 04:54:20 +0100

    Automatically invoke and exit the ldsc dependency

--- 1000G_ldsc/ldsc_chr.sh
+++ 1000G_ldsc/ldsc_chr.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+source activate ldsc
 location="eur"
 
 for chr in {1..22}; do
 	./ldsc/ldsc.py --out 1000G_ldsc_jpt_chr/$location.$chr --ld-wind-cm 1 --bfile 1000G_ldsc_eur_chr/ref.$location.$chr --l2
 done
 
+source activate ldsc
