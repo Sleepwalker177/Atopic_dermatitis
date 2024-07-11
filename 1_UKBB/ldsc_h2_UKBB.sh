3e696798f4afe51f8eb0fa61b9ca0a720036093e
Zichong Liu <144846450+Sleepwalker177@users.noreply.github.com>
2024-07-11 06:00:42 +0100

    Use the correct sample prevalence

--- 1_UKBB/ldsc_h2_UKBB.sh
+++ 1_UKBB/ldsc_h2_UKBB.sh
@@ -5,15 +5,15 @@ source activate ldsc
 ./ldsc/ldsc.py \
 	--h2 UKBB.sumstats.gz \
 	--ref-ld-chr ../1000G_ldsc/1000G_ldsc_eur_chr/eur.@ \
-	--out UKBB_h2_1 \
+	--out UKBB_h2_2 \
 	--w-ld-chr ../1000G_ldsc/1000G_ldsc_eur_chr/eur.@
 
 ./ldsc/ldsc.py \
 	--h2 UKBB.sumstats.gz \
 	--ref-ld-chr ../1000G_ldsc/1000G_ldsc_eur_chr/eur.@ \
-	--out UKBB_h2_liability_1 \
+	--out UKBB_h2_liability_2 \
 	--pop-prev 0.055 \
-	--samp-prev 0.075408197 \
+	--samp-prev 0.03007115009 \
 	--w-ld-chr ../1000G_ldsc/1000G_ldsc_eur_chr/eur.@
 
 conda deactivate
