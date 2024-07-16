0fe3ab361442260f8978c9d67bf63a9f874cc213
Zichong Liu <144846450+Sleepwalker177@users.noreply.github.com>
2024-07-16 03:23:46 +0100

    Fix the dir path

--- ldmatrix/genetic_map_jpt/clean_NA_rsID_genetic_map.sh
+++ ldmatrix/genetic_map_jpt/clean_NA_rsID_genetic_map.sh
@@ -5,11 +5,11 @@ for chr in {1..22}; do
 awk -F'\t' '$1 != "NA"' /genetic_map_tommo_GRCh37_rsID_chr/genetic_map_tommo_GRCh37_rsID_chr$chr.tsv > /genetic_map_tommo_GRCh37_rsID_chr_cleaned/genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv
 
 # check the number of "NA"
-cut -f 1 genetic_map_tommo_GRCh37_rsID_chr$chr.tsv | grep -o 'NA' | wc -l
-cut -f 1 genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv | grep -o 'NA' | wc -l
+cut -f 1 /genetic_map_tommo_GRCh37_rsID_chr/genetic_map_tommo_GRCh37_rsID_chr$chr.tsv | grep -o 'NA' | wc -l
+cut -f 1 /genetic_map_tommo_GRCh37_rsID_chr_cleaned/genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv | grep -o 'NA' | wc -l
 
 # check the number of line
-wc -l genetic_map_tommo_GRCh37_rsID_chr$chr.tsv
-wc -l genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv
+wc -l /genetic_map_tommo_GRCh37_rsID_chr/genetic_map_tommo_GRCh37_rsID_chr$chr.tsv
+wc -l /genetic_map_tommo_GRCh37_rsID_chr_cleaned/genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv
 
 done
