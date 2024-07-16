0f0aabead2d43f95db74b73666facec0b93875f2
Zichong Liu <144846450+Sleepwalker177@users.noreply.github.com>
2024-07-16 03:39:20 +0100

    Fix the path problem

--- ldmatrix/genetic_map_jpt/clean_NA_rsID_genetic_map.sh
+++ ldmatrix/genetic_map_jpt/clean_NA_rsID_genetic_map.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
+input_dir="$HOME/Atopic_dermatitis/ldmatrix/genetic_map_jpt/genetic_map_tommo_GRCh37_rsID_chr"
+output_dir="$HOME/Atopic_dermatitis/ldmatrix/genetic_map_jpt/genetic_map_tommo_GRCh37_rsID_chr_cleaned"
+
+mkdir -p $output_dir
+
 for chr in {1..22}; do
+  input_file="$input_dir/genetic_map_tommo_GRCh37_rsID_chr${chr}.tsv"
+  output_file="$output_dir/genetic_map_tommo_GRCh37_rsID_cleaned_chr${chr}.tsv"
 
-awk -F'\t' '$1 != "NA"' /genetic_map_tommo_GRCh37_rsID_chr/genetic_map_tommo_GRCh37_rsID_chr$chr.tsv > /genetic_map_tommo_GRCh37_rsID_chr_cleaned/genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv
+  if [ ! -f "$input_file" ]; then
+    echo "Error: $input_file not found."
+    continue
+  fi
 
-# check the number of "NA"
-cut -f 1 /genetic_map_tommo_GRCh37_rsID_chr/genetic_map_tommo_GRCh37_rsID_chr$chr.tsv | grep -o 'NA' | wc -l
-cut -f 1 /genetic_map_tommo_GRCh37_rsID_chr_cleaned/genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv | grep -o 'NA' | wc -l
+  awk -F'\t' '$1 != "NA"' "$input_file" > "$output_file"
 
-# check the number of line
-wc -l /genetic_map_tommo_GRCh37_rsID_chr/genetic_map_tommo_GRCh37_rsID_chr$chr.tsv
-wc -l /genetic_map_tommo_GRCh37_rsID_chr_cleaned/genetic_map_tommo_GRCh37_rsID_cleaned_chr$chr.tsv
+  # check the number of NA
+  na_count_before=$(cut -f 1 "$input_file" | grep -o 'NA' | wc -l)
+  na_count_after=$(cut -f 1 "$output_file" | grep -o 'NA' | wc -l)
 
+  # check the number of row
+  line_count_before=$(wc -l < "$input_file")
+  line_count_after=$(wc -l < "$output_file")
+
+  # output
+  echo "Chromosome $chr:"
+  echo "NA count before: $na_count_before"
+  echo "NA count after:  $na_count_after"
+  echo "Line count before: $line_count_before"
+  echo "Line count after:  $line_count_after"
+  echo ""
 done
