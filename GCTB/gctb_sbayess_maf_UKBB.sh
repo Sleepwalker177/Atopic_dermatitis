86333124ea534cf3075c7bd8126f9a92a861fbe9
Zichong Liu <144846450+Sleepwalker177@users.noreply.github.com>
2024-08-16 11:24:24 +0100

    Analyse the data that has passed different MAF filters

---
+++ GCTB/gctb_sbayess_maf_UKBB.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+thresholds=("0.001" "0.01" "0.05")
+
+for threshold in "${thresholds[@]}"; do
+
+gwas_path_UKBB=../1_UKBB/gctb_UKBB_maf${threshold}.ma
+gwas_path_NealeLab=../2_NealeLab/gctb_NealeLab_maf${threshold}.ma
+gwas_path_FinnGen=../3_FinnGen/gctb_FinnGen_maf${threshold}.ma
+gwas_path_Riken=../4_Riken/gctb_Riken_maf${threshold}.ma
+
+# Calling SBayesR
+gctb --sbayes S \
+     --ldm ../ldmatrix/ukbEURu_imp_v3_HM3_n50k_chisq10_sperse/ukbEURu_imp_v3_HM3_n50k.chisq10.ldm.sparse \
+     --gwas-summary $gwas_path_UKBB \
+     --exclude-mhc \
+     --chain-length 10000 \
+     --burn-in 2000 \
+     --out-freq 10 \
+     --annot ../Functional_genomic_annotations/annot_baseline2.2_21main.tsv \
+     --out gctb_UKBB_mhc_null_maf${threshold}_annot
+
+# Exclude MHC loci from calculations by including the option --exclude-mhc
+gctb --sbayes S \
+     --ldm ../ldmatrix/ukbEURu_imp_v3_HM3_n50k_chisq10_sperse/ukbEURu_imp_v3_HM3_n50k.chisq10.ldm.sparse \
+     --gwas-summary $gwas_path_UKBB \
+     --chain-length 10000 \
+     --burn-in 2000 \
+     --out-freq 10 \
+     --annot ../Functional_genomic_annotations/annot_baseline2.2_21main.tsv \
+     --out gctb_UKBB_mhc_maf${threshold}_annot
+done
